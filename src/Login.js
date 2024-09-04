baecf6f41f7fd23343d039ecc5ee180712957177
Suresh <sureshabde172@gmail.com>
2024-09-04 10:35:27 -0700

    WEbsite

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div><h1>Instructions</h1><br></br>
+    <ol>
+      <li>Weekly once new video uploaded</li><br></br>
+      <li>Categories Wised video uploaded</li><br></br>
+      <li>Give your opinion </li><br></br>
+      <li>Provide your valuable feedback</li><br></br>
+      <li>Give your Feedback to the given below form</li>
+    </ol>
+ </div>
   )
 }
 
